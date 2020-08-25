6c271a239e7236cdc9b14b77c259ee2b382fa082
stefanatanasov1 <stefanatanasov1@users.github.com>
2020-08-25 22:22:18 +0300

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,6 +36,14 @@ namespace GradeBook.GradeBooks
            else return 'F';
 
       }
-     
+
+      public override void CalculateStatistics() 
+      {
+       if(Students.Count < 5)
+          Console.WriteLine ("Ranked grading requires at least  5 students with grades in order to properly calculate a student's overall grade."); 
+      
+       else if(Students.Count >= 5)
+          base.CalculateStatistics();
+      }
     }
-}
+} 

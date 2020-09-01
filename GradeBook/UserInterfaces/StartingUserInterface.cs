8fd5f49cf0d45c26763a793dcf87918004b0fbd6
stefanatanasov1 <stefanatanasov1@users.github.com>
2020-09-01 17:24:19 +0300

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,7 +41,7 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            BaseGradeBook gradeBook;
+            BaseGradeBook gradeBook = new BaseGradeBook(name);
             if(type == "standart") 
               gradeBook = new StandartGradeBook(name);
             else if (type == "ranked")

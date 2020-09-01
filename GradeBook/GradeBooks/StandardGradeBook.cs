f87be36541cdf429c470ab98fd652110cab3fbe8
stefanatanasov1 <stefanatanasov1@users.github.com>
2020-09-01 17:26:10 +0300

    Update StandardGradeBook.cs

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -7,7 +7,7 @@ namespace GradeBook.GradeBooks
         private string name;
 
         public StandardGradeBook (string name) : base(name)
-        {  
+        {  Name = name;
            Type = GradeBookType.Standard;
         }
         

23658ed02c4d3ff6be75b4d7129ee0d8dd6c9e87
stefanatanasov1 <stefanatanasov1@users.github.com>
2020-09-01 17:21:16 +0300

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,18 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2].ToLower();
+            BaseGradeBook gradeBook;
+            if(type == "standart") 
+              gradeBook = new StandartGradeBook(name);
+            else if (type == "ranked")
+            gradeBook = new RankedGradeBook(name); 
+            else 
+            {
+              Console.WriteLine("{0}  is not a supported type of gradebook, please try again.",type);
+              return;
+            }
+            
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
